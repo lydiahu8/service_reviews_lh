c4d74dc39b4e59b927188d5fb5e4b32db7828793
lydiahu8 <lydiahu8@gmail.com>
2019-02-28 15:52:28 -0800

    Add config file for PostgreSQL

---
+++ database/config.example.js
@@ -0,0 +1,8 @@
+// Make a copy of this file, rename it to config.js, and use it to connect to mysql
+
+module.exports = {
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'products',
+};
