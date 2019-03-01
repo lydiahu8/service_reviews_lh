a4365fb255f2d647835f7c8edb0a81a3a842591c
lydiahu8 <lydiahu8@gmail.com>
2019-02-28 16:01:55 -0800

    Add subRatings table to schema

--- reviews.sql
+++ reviews.sql
@@ -6,21 +6,28 @@ CREATE DATABASE products;
 
 CREATE TABLE IF NOT EXISTS reviews (
   review_id SERIAL PRIMARY KEY,
-  product_id VARCHAR(10),
-  product_name VARCHAR(100),
-  user_id INTEGER,
-  username VARCHAR(100),
-  ratings VARCHAR(2),
+  product_id VARCHAR(10) NOT NULL,
+  product_name VARCHAR(100) NOT NULL,
+  user_id INTEGER NOT NULL,
+  username VARCHAR(100) NOT NULL,
+  overall_ratings VARCHAR(1),
   headline VARCHAR(255),
   review TEXT,
-  created INTEGER,
-  updated INTEGER,
+  created DATE DEFAULT CURRENT_DATE NOT NULL,
+  updated DATE DEFAULT CURRENT_DATE NOT NULL,
   verified BOOLEAN,
   helpful INTEGER
 );
 
 CREATE TABLE IF NOT EXISTS images (
   image_id SERIAL PRIMARY KEY,
-  image_url VARCHAR(255),
+  image_url VARCHAR(255) NOT NULL,
+  review_id INTEGER REFERENCES reviews(review_id)
+);
+
+CREATE TABLE IF NOT EXISTS subRatings (
+  subRating_id SERIAL PRIMARY KEY,
+  feature VARCHAR(50),
+  rating VARCHAR(1)
   review_id INTEGER REFERENCES reviews(review_id)
 );
